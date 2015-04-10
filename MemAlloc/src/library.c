4226115d2a0b2339f8ff706c8e1fb6a8876ed360
cnehem <cnehem@lab2-7.CS.McGill.CA>
2015-04-10 19:44:29 -0400

    Something working?

--- MemAlloc/src/library.c
+++ MemAlloc/src/library.c
@@ -6,57 +6,97 @@
  */
 
 #include "mem_alloc_api.h"
-#define NULL 0
+#include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <assert.h>
 
-typedef struct blck{
-	int length;// length of the block
-	int mem;//memory for use by caller
-}block;
+#define BLOCK_SIZE sizeof(blocks)
 
 typedef struct f_block{
-	int length;// length of the block
-	block prev;// pointer to previous block
-	block next;// pointer to next block
-	int rem;//amount of remaining memory
-}free_blocks;
+	int length;// length of block
+	bool inUse;// whether block is free or not
+	struct f_block *prev;// pointer to previous block
+	struct f_block *next;// pointer to next block
+	int address;//address where it located
+}blocks;
 
-typedef struct bound{
-	int length;// length of tag
-	int inUse;// 1 for in use , 0 for free
-}top_boundary;
-
-typedef struct bound{
-	int length;//length to indicate where the top is
-}low_boundary;
-
+extern char *my_malloc_error;
 
 /*global variables*/
-top_boundary top;
-low_boundary bottom;
-free_blocks free;
-int program_break;//location of the program break
+void *global_base = NULL;// pointer to the head
+int frag;// value representing threshold of internal fragmentation
+int newPolicy;// the memory allocation policy to be used
 
+blocks *find_free_block(blocks **last,int size){
 
-void *my_malloc(int size){
-	while(free.next != NULL){
+	blocks *current = global_base;
 
-		if(size > free.length){
-			free = free.next;
-		}
-		else{
-			free.prev.length = size;
-			sbrk(size);
-		}
+	while(current && !(current->inUse && current->length >= size)){
+		*last = current;
+		current = current->next;
 	}
+
+	return current;
 }
 
-void my_free(void *ptr){
+blocks *request_space(blocks *last, int size){
 
-	if(*ptr == NULL)
+	blocks *blk;
+
+	blk = sbrk(0);
+
+	void *request = sbrk(size + BLOCK_SIZE);
+
+	assert((void*)blk == request);
+
+	if (request == (void*) -1) {
+	    return NULL; // sbrk failed.
+	  }
+
+	  if (last) { // NULL on first request.
+	    last->next = blk;
+	  }
+	  blk->length = size;
+	  blk->next = NULL;
+	  blk->inUse = false;
+	  blk->address = 0x12345678;
+
+	  return blk;
+}
+
+void *my_malloc(int size){
+
+	blocks *free;//list of free blocks
+
+	if(size <=0){ // requesting no space?
+		return NULL;
+	}
+
+	if(!global_base){
+
+		free = request_space(NULL,size);
+		if(!free){ // unable to allocate space
+			return NULL;
+		}
+		global_base = free;
+	}
+	else
 	{
-		return;
+		blocks *last = global_base;
+		free = find_free_block(&last,size);
+		if(!free){
+			free = request_space(last,size);
+			if(!free){
+				return NULL; // space could not be allocated
+			}
+		}
+		else // free block found
+		{
+			free->inUse = true;
+			free->address = 0x77777777;
+		}
 	}
-	if(top.length > 128){
-		sbrk(top.length);
-	}
+
+	return (free+1); // return pointer to region after the meta information of the block
 }
